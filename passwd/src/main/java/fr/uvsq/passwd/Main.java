fa0ef99e227966e1fba772634702125950df987b
Marek Felšöci <marek@felsoci.sk>
2026-04-01 17:13:23 +0200

    Implémenter un programme principal

--- passwd/src/main/java/fr/uvsq/passwd/Main.java
+++ passwd/src/main/java/fr/uvsq/passwd/Main.java
@@ -1,10 +1,18 @@
 package fr.uvsq.passwd;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Bienvenue dans le module d'évaluation de mots de passe !");
+    System.out.print("Saisissez un mot de passe : ");
+    Scanner passwordScanner = new Scanner(System.in);
+    String password = passwordScanner.next();
+    Strength strength = Evaluation.evaluatePassword(password);
+    if(strength == Strength.STRONG) {
+      System.out.println("Félicitations ! Votre mot de passe est robuste !");
+    } else {
+      System.out.println("Vous devriez envisager de changer de mot de passe.");
     }
+  }
 }
